fa957791ff633043d26b228626c3f4d98cda29ec
hooloong <hooloongge@sigmadesigns.com>
2016-04-19 18:31:41 +0800

    check color on qt4

--- twoFirst01/QTGUI09_dailog.py
+++ twoFirst01/QTGUI09_dailog.py
@@ -23,9 +23,39 @@ class InputDailog(QtGui.QWidget):
         text,ok = QtGui.QInputDialog.getText(self,'Input Dailog','Enter your name')
         if ok:
             self.label.setText(unicode(text))
+
+class ColorDialog(QtGui.QWidget):
+    def __init__(self,parent=None):
+        QtGui.QWidget.__init__(self,parent)
+        self.setWindowTitle("Color dailog")
+        color = QtGui.QColor(0,0,0)
+        self.setGeometry(300,300,250,180)
+
+        self.button = QtGui.QPushButton('Dialog',self)
+        self.button.setFocusPolicy(QtCore.Qt.NoFocus)
+        self.button.move(20,20)
+
+        self.connect(self.button,QtCore.SIGNAL('clicked()'),self.showDialog)
+        self.setFocus()
+        print "%s " % color.name()
+        self.widget = QtGui.QWidget(self)
+        self.widget.setStyleSheet('QWidget {background-color:%s}' % color.name())
+        self.widget.setGeometry(130,22,100,100)
+
+        # self.resize(350,300)
+
+    def showDialog(self):
+        col = QtGui.QColorDialog.getColor()
+        print "%s" % col.name()
+        if col.isValid():
+             self.widget.setStyleSheet('QWidget {background-color:%s}' % col.name())
+
+
+
 app = QtGui.QApplication( sys.argv )
 
-qb = InputDailog()
+# qb = InputDailog()
+qb = ColorDialog()
 qb.show()
 sys.exit(app.exec_())
 
