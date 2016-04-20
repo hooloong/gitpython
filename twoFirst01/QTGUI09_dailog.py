d7069d40502b4a39b63157bd1352dc9b76961cde
hooloong <hooloongge@sigmadesigns.com>
2016-04-20 10:37:56 +0800

    updated font and file dailog checking.

--- twoFirst01/QTGUI09_dailog.py
+++ twoFirst01/QTGUI09_dailog.py
@@ -50,12 +50,68 @@ class ColorDialog(QtGui.QWidget):
         if col.isValid():
              self.widget.setStyleSheet('QWidget {background-color:%s}' % col.name())
 
+class FileDailog(QtGui.QMainWindow):
+    def __init__(self,parent=None):
+        QtGui.QWidget.__init__(self,parent)
+        self.setWindowTitle("File dailog")
+
+        self.setGeometry(300,300,250,180)
+
+        self.textedit = QtGui.QTextEdit()
+        self.setCentralWidget(self.textedit)
+        self.statusBar()
+        self.setFocus()
+
+        exit = QtGui.QAction(QtGui.QIcon("222.ico"),'Open',self)
+        exit.setShortcut('Ctrl+O')
+        exit.setStatusTip('Open new file')
+
+        self.connect(exit,QtCore.SIGNAL('triggered()'),self.showDialog)
+
+        menubar = self.menuBar()
+        file = menubar.addMenu('&File')
+        file.addAction(exit)
+
+    def showDialog(self):
+        filename = QtGui.QFileDialog.getOpenFileName(self,'Open file','./')
+        file = open(filename)
+        data = file.read()
+        self.textedit.setText(data)
+
+class FontDailog(QtGui.QWidget):
+    def __init__(self,parent=None):
+        QtGui.QWidget.__init__(self,parent)
+        self.setWindowTitle("font dailog")
+        self.setGeometry(300,300,250,180)
+        hbox = QtGui.QHBoxLayout()
+        self.button = QtGui.QPushButton('font',self)
+        self.button.setFocusPolicy(QtCore.Qt.NoFocus)
+        self.button.move(20,20)
+        hbox.addWidget(self.button)
+        self.connect(self.button,QtCore.SIGNAL('clicked()'),self.showDialog)
+        self.setFocus()
+
+        self.label = QtGui.QLabel("Knowlege only matterss",self)
+        self.label.move(130,20)
+
+        hbox.addWidget(self.label,1)
+        self.setLayout(hbox)
+
+        # self.resize(350,300)
+
+    def showDialog(self):
+        font,ok = QtGui.QFontDialog.getFont()
+        print "font:%s" % font
+        if ok:
+             self.label.setFont(font)
 
 
 app = QtGui.QApplication( sys.argv )
 
 # qb = InputDailog()
-qb = ColorDialog()
+# qb = ColorDialog()
+# qb = FileDailog()
+qb = FontDailog()
 qb.show()
 sys.exit(app.exec_())
 
