64b0538ca739568ccf97cbf821ec1f05de1c4967
hooloong <hooloongge@sigmadesigns.com>
2016-04-20 21:53:44 +0800

    updated tablewidget setting

--- twoFirst01/DimmingDebugger.py
+++ twoFirst01/DimmingDebugger.py
@@ -1,9 +1,9 @@
 __author__ = 'hooloongge'
 import sys
 import ctypes as C
-import two01 as TFC
+# import two01 as TFC
 import xmltodict
-from dispmipsfunc import *
+# from dispmipsfunc import *
 from PyQt4 import  QtCore, QtGui,uic
 class MyWindow( QtGui.QMainWindow ):
     def __init__( self ):
@@ -15,8 +15,10 @@ class MyWindow( QtGui.QMainWindow ):
         self.connectFlag = 0
         self.connect(self.actionConnect,QtCore.SIGNAL('triggered()'),self.connectChip)
         self.connect(self.actionDisconnect,QtCore.SIGNAL('triggered()'),self.disconnectChip)
+        self.connect(self.pushButton_readpwm,QtCore.SIGNAL('clicked()'),self.readPWM)
+        self.connect(self.pushButton_readcurrent,QtCore.SIGNAL('clicked()'),self.readCurrent)
     def connectChip(self):
-        self.connectFlag = TFC.TFCConnect2Chip()
+        # self.connectFlag = TFC.TFCConnect2Chip()
         print("tfcConnInit returns ",self.connectFlag)
         if self.connectFlag:
              print("Connect to chip!!! ")
@@ -28,8 +30,14 @@ class MyWindow( QtGui.QMainWindow ):
             self.setWindowTitle("Dimming Debugger     DisConnect..")
     def disconnectChip(self):
         self.connectFlag = False
-        TFC.tfcConnTerm()
+        # TFC.tfcConnTerm()
         self.setWindowTitle("Dimming Debugger     DisConnect..")
+    def readPWM(self):
+        #added read from registers
+        newItem = QtGui.QTableWidgetItem('100')
+        self.tableWidget_PWM.setItem(0,0,newItem)
+    def readCurrent(self):
+        return
     def closeEvent(self,event):
         reply = QtGui.QMessageBox.question(self, 'Message', "Are you sure to quit?", QtGui.QMessageBox.Yes, QtGui.QMessageBox.No)
         if reply== QtGui.QMessageBox.Yes:
