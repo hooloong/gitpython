7a7a1a9e2b36290e367bf2a522270894c85fc493
hooloong <hooloong@vip.qq.com>
2016-04-27 17:11:43 +0800

    updated debugger tool: read 64 vars from chips. PWM is right.

--- twoFirst01/DimmingDebugger.py
+++ twoFirst01/DimmingDebugger.py
@@ -6,21 +6,23 @@ import two01 as TFC
 import xmltodict
 import json
 import time
-#import iniparse
+# import iniparse
 import ConfigParser
 import numpy as np
 from dispmipsfunc import *
-from PyQt4 import  QtCore, QtGui,uic,Qwt5
-class MyWindow( QtGui.QMainWindow ):
-    def __init__( self ):
-        super( MyWindow, self ).__init__()
-        uic.loadUi( "dimmingdebugger.ui", self )
+from PyQt4 import QtCore, QtGui, uic, Qwt5
+
+
+class MyWindow(QtGui.QMainWindow):
+    def __init__(self):
+        super(MyWindow, self).__init__()
+        uic.loadUi("dimmingdebugger.ui", self)
         self.setWindowIcon(QtGui.QIcon("222.ico"))
         # self.setGeometry(300,300,810,640)
         self.statusBar().showMessage('DisConnected to chip!!!')
         self.painter = QtGui.QPainter()
 
-        self.s_dict = dict(defaultfilename = "parameters.json")
+        self.s_dict = dict(defaultfilename="parameters.json")
         # self.s_dict =
         self.connectFlag = 0
         self.pwm = np.zeros(32)
@@ -34,56 +36,61 @@ class MyWindow( QtGui.QMainWindow ):
             for j in range(self.tableWidget_PWM.columnCount()):
                 newItemt = QtGui.QTableWidgetItem('0')
                 newItemt1 = QtGui.QTableWidgetItem('0')
-                self.tableWidget_PWM.setItem(i,j,newItemt)
-                self.tableWidget_PWM_2.setItem(i,j,newItemt1)
+                self.tableWidget_PWM.setItem(i, j, newItemt)
+                self.tableWidget_PWM_2.setItem(i, j, newItemt1)
         for i in range(self.tableWidget_Current.rowCount()):
             for j in range(self.tableWidget_Current.columnCount()):
                 newItemt = QtGui.QTableWidgetItem('0')
                 newItemt1 = QtGui.QTableWidgetItem('0')
-                self.tableWidget_Current.setItem(i,j,newItemt)
-                self.tableWidget_Current_2.setItem(i,j,newItemt1)
+                self.tableWidget_Current.setItem(i, j, newItemt)
+                self.tableWidget_Current_2.setItem(i, j, newItemt1)
         #end table init
-        self.connect(self.actionConnect,QtCore.SIGNAL('triggered()'),self.connectChip)
-        self.connect(self.actionDisconnect,QtCore.SIGNAL('triggered()'),self.disconnectChip)
-        self.connect(self.pushButton_readpwm,QtCore.SIGNAL('clicked()'),self.readPWM)
-        self.connect(self.pushButton_readpwm,QtCore.SIGNAL('pressed()'),self.changeText_pwm)
-        self.connect(self.pushButton_readcurrent,QtCore.SIGNAL('pressed()'),self.changeText_current)
-        self.connect(self.pushButton_readcurrent,QtCore.SIGNAL('clicked()'),self.readCurrent)
-        self.connect(self.pushButton_flushimg,QtCore.SIGNAL('clicked()'),self.update)
-        self.connect(self.pushButton_A1,QtCore.SIGNAL('clicked()'),self.Algo_1)
-        self.connect(self.pushButton_A2,QtCore.SIGNAL('clicked()'),self.Algo_2)
-        self.connect(self.pushButton_A3,QtCore.SIGNAL('clicked()'),self.Algo_3)
-        self.actionQuit.connect(self.actionQuit,QtCore.SIGNAL('triggered()'),QtGui.qApp,QtCore.SLOT('quit()'))
-        self.actionOpen_setting_file.connect(self.actionOpen_setting_file,QtCore.SIGNAL('triggered()'),self.loadSettingfromJson)
-        self.actionSave_setting_file.connect(self.actionSave_setting_file,QtCore.SIGNAL('triggered()'),self.saveSettingfromJson)
+        self.connect(self.actionConnect, QtCore.SIGNAL('triggered()'), self.connectChip)
+        self.connect(self.actionDisconnect, QtCore.SIGNAL('triggered()'), self.disconnectChip)
+        self.connect(self.pushButton_readpwm, QtCore.SIGNAL('clicked()'), self.readPWM)
+        self.connect(self.pushButton_readpwm, QtCore.SIGNAL('pressed()'), self.changeText_pwm)
+        self.connect(self.pushButton_readcurrent, QtCore.SIGNAL('pressed()'), self.changeText_current)
+        self.connect(self.pushButton_readcurrent, QtCore.SIGNAL('clicked()'), self.readCurrent)
+        self.connect(self.pushButton_flushimg, QtCore.SIGNAL('clicked()'), self.update)
+        self.connect(self.pushButton_A1, QtCore.SIGNAL('clicked()'), self.Algo_1)
+        self.connect(self.pushButton_A2, QtCore.SIGNAL('clicked()'), self.Algo_2)
+        self.connect(self.pushButton_A3, QtCore.SIGNAL('clicked()'), self.Algo_3)
+        self.actionQuit.connect(self.actionQuit, QtCore.SIGNAL('triggered()'), QtGui.qApp, QtCore.SLOT('quit()'))
+        self.actionOpen_setting_file.connect(self.actionOpen_setting_file, QtCore.SIGNAL('triggered()'),
+                                             self.loadSettingfromJson)
+        self.actionSave_setting_file.connect(self.actionSave_setting_file, QtCore.SIGNAL('triggered()'),
+                                             self.saveSettingfromJson)
 
         self.loadSettingfromJson()
-    def paintEvent(self,envent):
+
+    def paintEvent(self, envent):
         self.painter.begin(self)
         startposx = self.tableWidget_Paras.x()
-        startposy = self.tableWidget_Paras.y()+self.tableWidget_Paras.height()+60
+        startposy = self.tableWidget_Paras.y() + self.tableWidget_Paras.height() + 60
         for i in range(8):
             for j in range(8):
-                var = self.pwm[j*self.tableWidget_Current.rowCount() + i]
+                var = self.pwm[j * self.tableWidget_Current.rowCount() + i]
                 br = (var & 0xFFF) >> 4
-                color = QtGui.QColor(br,br,br)
+                color = QtGui.QColor(br, br, br)
                 self.painter.setBrush(color)
-                self.painter.drawRect(startposx+i*40,startposy+j*30,40,30)
+                self.painter.drawRect(startposx + i * 40, startposy + j * 30, 40, 30)
         self.painter.end()
+
     def connectChip(self):
         config_read = ConfigParser.RawConfigParser()
         config_read.read("ChipDebugger.INI")
-        eth_ip = config_read.get("Connection","Ethernet.IP")
+        eth_ip = config_read.get("Connection", "Ethernet.IP")
         self.connectFlag = TFC.TFCConnect2Chip()
-        print("tfcConnInit returns ",self.connectFlag)
+        print("tfcConnInit returns ", self.connectFlag)
         if self.connectFlag:
-             print("Connect to chip!!! ")
-             self.statusBar().showMessage('"Connect to chip!!!')
-             self.setWindowTitle("Dimming Debugger" + "   " +"Connection" + "   " + eth_ip)
+            print("Connect to chip!!! ")
+            self.statusBar().showMessage('"Connect to chip!!!')
+            self.setWindowTitle("Dimming Debugger" + "   " + "Connection" + "   " + eth_ip)
         else:
             print("Could not Connect to chip!!! ")
-            self.statusBar().showMessage('Could not Connect to ' + "   " +eth_ip + "!!!!" )
+            self.statusBar().showMessage('Could not Connect to ' + "   " + eth_ip + "!!!!")
             self.setWindowTitle("Dimming Debugger" + "    " + "DisConnection")
+
     def disconnectChip(self):
         if self.connectFlag == False:
             return
@@ -99,15 +106,16 @@ class MyWindow( QtGui.QMainWindow ):
         for key in self.s_dict:
             # print key, self.s_dict[key]
             newItemh = QtGui.QTableWidgetItem(key)
-            self.tableWidget_Paras.setVerticalHeaderItem(j,newItemh)
+            self.tableWidget_Paras.setVerticalHeaderItem(j, newItemh)
             str = "%d" % self.s_dict[key]
             newItemi = QtGui.QTableWidgetItem(str)
-            self.tableWidget_Paras.setItem(j,0,newItemi)
-            i = i+1
-            j = j +1
+            self.tableWidget_Paras.setItem(j, 0, newItemi)
+            i = i + 1
+            j = j + 1
+
     def loadSettingfromJson(self):
         settingfile = "parameters.json"
-        s_fp = open(settingfile,'r')
+        s_fp = open(settingfile, 'r')
         if s_fp == False:
             print "error file!!!!"
         else:
@@ -117,9 +125,10 @@ class MyWindow( QtGui.QMainWindow ):
             print self.s_setjson
             print self.s_dict
             self.loadParaTable()
+
     def saveSettingfromJson(self):
         settingfile = "parameters1.json"
-        s_fp = open(settingfile,'w+')
+        s_fp = open(settingfile, 'w+')
         if s_fp == False:
             print "error file!!!!"
         elif len(self.s_dict) == 1:
@@ -127,78 +136,132 @@ class MyWindow( QtGui.QMainWindow ):
         else:
             s_fp.write("%s" % self.s_setjson)
             s_fp.close()
+
     def changeText_pwm(self):
-        if self.connectFlag == False:
+        if self.connectFlag is False:
             return
         self.pushButton_readpwm.setText("Reading")
+
     def changeText_current(self):
         if self.connectFlag == False:
             return
         self.pushButton_readcurrent.setText("Reading")
+
     def readPWM(self):
         #added read from registers
-        if self.connectFlag == False:
+        if self.connectFlag is False:
             return
         for i in range(self.tableWidget_PWM.rowCount()):
             for j in range(self.tableWidget_PWM.columnCount()):
-                TFC.tfc2ddWriteDword(0x68,0xA5)
-                TFC.tfc2ddWriteDword(0x6C,i*self.tableWidget_PWM.rowCount() + j)
+                TFC.tfc2ddWriteDword(0x68, 0xA5)
+                TFC.tfc2ddWriteDword(0x6C, i * self.tableWidget_PWM.rowCount() + j)
                 time.sleep(0.05)
                 var = TFC.tfc2ddReadDword(0x68) >> 8;
-                self.pwm[i*self.tableWidget_PWM.rowCount() + j] = var & 0xFFF
+                self.pwm[i * self.tableWidget_PWM.rowCount() + j] = var & 0xFFF
                 pwmdutytemp = "%X" % (var & 0xFFF)
                 newItemt = QtGui.QTableWidgetItem(pwmdutytemp)
-                self.tableWidget_PWM.setItem(i,j,newItemt)
+                self.tableWidget_PWM.setItem(i, j, newItemt)
         self.pushButton_readpwm.setText("ReadPWM")
         # print self.pushButton_readpwm.text
         print self.pwm
+        self.update()
+
     def readCurrent(self):
-        if self.connectFlag == False:
+        if self.connectFlag is False:
             return
         for i in range(self.tableWidget_Current.rowCount()):
             for j in range(self.tableWidget_Current.columnCount()):
-                TFC.tfc2ddWriteDword(0x68,0xA5)
-                TFC.tfc2ddWriteDword(0x6C,i*self.tableWidget_Current.rowCount() + j)
+                TFC.tfc2ddWriteDword(0x68, 0xA5)
+                TFC.tfc2ddWriteDword(0x6C, i * self.tableWidget_Current.rowCount() + j)
                 time.sleep(0.1)
                 var = TFC.tfc2ddReadDword(0x68) >> 8;
-                self.current[i*self.tableWidget_Current.rowCount() + j] = var & 0xFFF
+                self.current[i * self.tableWidget_Current.rowCount() + j] = var & 0xFFF
                 pwmdutytemp = "%X" % (var & 0xFFF)
                 newItemt = QtGui.QTableWidgetItem(pwmdutytemp)
-                self.tableWidget_Current.setItem(i,j,newItemt)
+                self.tableWidget_Current.setItem(i, j, newItemt)
         self.pushButton_readcurrent.setText("ReadCurrent")
         # print self.pushButton_readcurrent.text
         print self.current
+
     def outputCurrent(self):
         for i in range(self.tableWidget_Current_2.rowCount()):
             for j in range(self.tableWidget_Current_2.columnCount()):
-                var = self.current[i*self.tableWidget_Current.rowCount() + j]
+                var = self.current[i * self.tableWidget_Current.rowCount() + j]
                 pwmdutytemp = "%X" % (var & 0xFFF)
                 newItemt = QtGui.QTableWidgetItem(pwmdutytemp)
-                self.tableWidget_Current_2.setItem(i,j,newItemt)
+                self.tableWidget_Current_2.setItem(i, j, newItemt)
         print self.current
+
     def outputPWM(self):
         for i in range(self.tableWidget_PWM_2.rowCount()):
             for j in range(self.tableWidget_PWM_2.columnCount()):
-                var = self.pwm[i*self.tableWidget_PWM_2.rowCount() + j]
+                var = self.pwm[i * self.tableWidget_PWM_2.rowCount() + j]
                 pwmdutytemp = "%X" % (var & 0xFFF)
                 newItemt = QtGui.QTableWidgetItem(pwmdutytemp)
-                self.tableWidget_PWM_2.setItem(i,j,newItemt)
+                self.tableWidget_PWM_2.setItem(i, j, newItemt)
+
     def Algo_1(self):
-        return;
+        peak_sum = 0
+        global_gain = 1024
+        reduce_pwm = 100
+        led_x = 8
+        led_y = 8
+        led_size = led_x * led_y
+        dc_max_sum = np.zeros((8, 5), np.uint32)
+        reduce_pwm_coef = np.ones((8, 3), np.uint32)
+        reduce_pwm_coef *= 100
+        dc_peak_num = np.zeros(8, np.uint32)
+        reduce_pwm_sum = 0
+        dc_mapping_3820_m70 = np.array(
+            [0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 3, 2, 2, 2, 2, 4, 4, 4, 4,
+             5, 5, 5, 5, 5, 5, 5, 5, 4, 4, 4, 4, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 7, 7, 6, 6, 6, 6], np.uint32)
+        # dc_mapping_3820_m70.shape = (8, 8)
+
+        for i in range(led_size):
+            tmp_i = dc_mapping_3820_m70[i]
+            if self.pwm[i] > self.s_dict["peak_value_thr"]:
+                dc_peak_num[tmp_i] += 1
+                peak_sum += 1
+        print peak_sum
+        print dc_peak_num
+
+
     def Algo_2(self):
         self.outputPWM()
         self.outputCurrent()
-        return
+
     def Algo_3(self):
-        return
-    def closeEvent(self,event):
-        reply = QtGui.QMessageBox.question(self, 'Message', "Are you sure to quit?", QtGui.QMessageBox.Yes, QtGui.QMessageBox.No)
-        if reply== QtGui.QMessageBox.Yes:
+        pass
+
+    def closeEvent(self, event):
+        reply = QtGui.QMessageBox.question(self, 'Message', "Are you sure to quit?", QtGui.QMessageBox.Yes,
+                                           QtGui.QMessageBox.No)
+        if reply == QtGui.QMessageBox.Yes:
             event.accept()
         else:
             event.ignore()
+
+
+class Worker(QtCore.QThread):
+    def __init__(self, parent=None):
+        super(Worker, self).__init__(parent)
+        self.working = True
+        self.num = 0
+
+    def __del__(self):
+        self.working = False
+        self.wait()
+
+    def run(self):
+        while self.working == True:
+            file_str = 'File index {0}'.format(self.num)
+            self.num += 1
+            self.emit(SIGNAL('output(QString)'), file_str)
+            self.sleep(3)
+
+
 if __name__ == "__main__":
-    app = QtGui.QApplication( sys.argv )
+    app = QtGui.QApplication(sys.argv)
     mywindow = MyWindow()
     mywindow.show()
     app.exec_()
