830d57e3c0d60cb111324c237117becec0046b20
hooloong <hooloong@vip.qq.com>
2017-03-30 20:43:17 +0800

    111111

--- twoFirst01/DimmingDebugger_Readback_SX7/Dimpages.py
+++ twoFirst01/DimmingDebugger_Readback_SX7/Dimpages.py
@@ -3,12 +3,27 @@ __author__ = 'hooloongge'
 import sys, math, random, time
 import ctypes as C
 import two01 as TFC
-# import xmltodict
 import numpy as np
 from dispmipsfunc import *
 from PyQt4 import QtCore, QtGui, uic  # ,Qwt5
 from Dimpages_res import *
 
+
+class HexSpinBox(QtGui.QSpinBox):
+
+    def __init__(self):
+        super(HexSpinBox, self).__init__()
+        self.setRange(0,0xFFFFFFFF)
+        self.validator = QtGui.QRegExpValidator(QtCore.QRegExp("[0-9A-Fa-f]{1,8}"))
+
+    def validate(self, text, pos):
+        return self.validator.validate(text,pos)
+
+    def textFromValue(self, value):
+        return QtCore.QString.number(value,16).toUpper()
+
+    def valueFromText(self, text):
+        return text.toInt(16)
 
 class DimPagesWindow(QtGui.QMainWindow):
 
@@ -16,6 +31,17 @@ class DimPagesWindow(QtGui.QMainWindow):
         super(DimPagesWindow, self).__init__()
         self.ui =  Ui_Form_Dimpages()
         self.ui.setupUi(self)
+        # insert hexspinbox in to table
+        hexspinlist = []
+        for i in range(64):
+            # newhex = HexSpinBox()
+            newhex = QtGui.QSpinBox()
+            hexspinlist.append(newhex)
+        for i in range(self.ui.tableWidget_curpage.columnCount()):
+            for j in range(self.ui.tableWidget_curpage.rowCount()):
+                self.ui.tableWidget_curpage.setCellWidget(j,i,hexspinlist[j*4 + i])
+
+
 
         self.connectFlag = False
 
