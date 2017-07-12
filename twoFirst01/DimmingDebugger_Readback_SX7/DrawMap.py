05d95d74196297cae24f070451d31658c03d93f8
hooloong <hooloongge@sigmadesigns.com>
2017-07-12 14:11:38 +0800

    Add DrawMapLut

---
+++ twoFirst01/DimmingDebugger_Readback_SX7/DrawMap.py
@@ -0,0 +1,253 @@
+
+__author__ = 'hooloongge'
+# -*- coding: utf-8 -*-
+import sys, math, random, time
+import ctypes as C
+import two01 as TFC
+import numpy as np
+from dispmipsfunc import *
+from PyQt4 import QtCore, QtGui, QtSql,uic  # ,Qwt5
+import pyqtgraph as pg
+from DrawMap_res import  *
+
+
+class Graph(pg.GraphItem):
+    def __init__(self):
+        self.dragPoint = None
+        self.dragOffset = None
+        self.textItems = []
+        pg.GraphItem.__init__(self)
+        self.scatter.sigClicked.connect(self.clicked)
+
+    def setData(self, **kwds):
+        self.text = kwds.pop('text', [])
+        self.data = kwds
+        if 'pos' in self.data:
+            npts = self.data['pos'].shape[0]
+            self.data['data'] = np.empty(npts, dtype=[('index', int)])
+            self.data['data']['index'] = np.arange(npts)
+        self.setTexts(self.text)
+        self.updateGraph()
+
+    def setTexts(self, text):
+        for i in self.textItems:
+            i.scene().removeItem(i)
+        self.textItems = []
+        for t in text:
+            item = pg.TextItem(t)
+            self.textItems.append(item)
+            item.setParentItem(self)
+
+    def updateGraph(self):
+        pg.GraphItem.setData(self, **self.data)
+        for i,item in enumerate(self.textItems):
+            item.setPos(*self.data['pos'][i])
+
+
+    def mouseDragEvent(self, ev):
+        if ev.button() != QtCore.Qt.LeftButton:
+            ev.ignore()
+            return
+
+        if ev.isStart():
+            # We are already one step into the drag.
+            # Find the point(s) at the mouse cursor when the button was first
+            # pressed:
+            pos = ev.buttonDownPos()
+            pts = self.scatter.pointsAt(pos)
+            if len(pts) == 0:
+                ev.ignore()
+                return
+            self.dragPoint = pts[0]
+            ind = pts[0].data()[0]
+            self.dragOffset = self.data['pos'][ind] - pos
+        elif ev.isFinish():
+            self.dragPoint = None
+            return
+        else:
+            if self.dragPoint is None:
+                ev.ignore()
+                return
+
+        ind = self.dragPoint.data()[0]
+        self.data['pos'][ind] = ev.pos() + self.dragOffset
+        self.updateGraph()
+        ev.accept()
+
+    def clicked(self, pts):
+        print("clicked: %s" % pts)
+
+
+
+## Define positions of nodes
+pos = np.array([
+    [0,0],
+    [10,0],
+    [0,10],
+    [10,10],
+    [5,5],
+    [15,5]
+    ], dtype=float)
+
+## Define the set of connections in the graph
+adj = np.array([
+    [0,1],
+    [1,3],
+    [3,2],
+    [2,0],
+    [1,5],
+    [3,5],
+    ])
+
+## Define the symbol to use for each node (this is optional)
+symbols = ['o','o','o','o','o','o']
+
+## Define the line style for each connection (this is optional)
+lines = np.array([
+    (255,0,0,255,1),
+    (255,0,255,255,1),
+    (255,0,255,255,1),
+    (255,255,0,255,1),
+    (255,0,0,255,1),
+    (255,255,255,255,1),
+    ], dtype=[('red',np.ubyte),('green',np.ubyte),('blue',np.ubyte),('alpha',np.ubyte),('width',float)])
+
+## Define text to show next to each symbol
+texts = ["%d" % i for i in range(192)]
+for i in range(5):
+    pos = np.row_stack((pos,pos))
+    adj =  np.row_stack((adj,adj))
+    symbols = symbols *2
+# lines = lines.repeat(32)
+lines =  np.tile(lines,32)
+for i in range(192):
+    pos[i][0] = i
+    pos[i][1] = (192-i) * 0xC00/0x400
+    adj[i][0] = i
+    adj[i][1] = i+1
+adj[191][1] = 191
+print len(pos),len(adj),len(lines),len(symbols)
+
+class CustomViewBox(pg.ViewBox):
+    def __init__(self, *args, **kwds):
+        pg.ViewBox.__init__(self, *args, **kwds)
+        # self.setMouseMode(self.RectMode)
+
+        self.setLimits(xMin=0,yMin=0x400)
+    ## reimplement right-click to zoom out
+    def mouseClickEvent(self, ev):
+        if ev.button() == QtCore.Qt.RightButton:
+            self.autoRange()
+        pass
+
+    def mouseDragEvent(self, ev):
+        if ev.button() == QtCore.Qt.RightButton:
+            ev.ignore()
+        else:
+          pg.ViewBox.mouseDragEvent(self, ev)
+        pass
+
+
+
+class CustomPolyLineROI2(pg.PolyLineROI):
+
+    def __init__(self, positions, closed=False, pos=None, **args):
+        pg.PolyLineROI.__init__(self, positions, closed=False, pos=None,movable=False, **args)
+
+    def segmentClicked(self, segment, ev=None, pos=None): ## pos should be in this item's coordinate system
+        if ev != None:
+            pos = segment.mapToParent(ev.pos())
+        elif pos != None:
+            pos = pos
+        else:
+            raise Exception("Either an event or a position must be given.")
+
+    # def paint(self, p, *args):
+    #     p.setRenderHint(QtGui.QPainter.Antialiasing)
+    #     p.setPen(self.currentPen)
+    #     h1 = self.handles[0]['item'].pos()
+    #     h2 = self.handles[1]['item'].pos()
+    #     p.drawCubicBezier(h1, h2)
+    # def shape(self):
+    #     p = QtGui.QPainterPath()
+    #     if len(self.handles) == 0:
+    #         return p
+    #     p.moveTo(self.handles[0]['item'].pos())
+    #     for i in range(len(self.handles)):
+    #         p.lineTo(self.handles[i]['item'].pos())
+    #     p.lineTo(self.handles[0]['item'].pos())
+    #     print p
+    #     return p
+
+class DrawL2GWindow(QtGui.QMainWindow):
+
+    def __init__(self):
+        super(DrawL2GWindow, self).__init__()
+        self.ui =  Ui_Form_Hist()
+        self.ui.setupUi(self)
+
+        self.lgdata = []
+        pg.setConfigOptions(antialias=True)
+        # #self.w = pg.GraphicsWindow()
+        # self.w = pg.GraphicsLayoutWidget()
+        # self.ui.gridLayout_2.addWidget(self.w, 0,0)
+        # self.v = self.w.addViewBox()
+        # self.v.setAspectLocked()
+        # self.gld = Graph()
+        # self.v.addItem(self.gld)
+        # self.gld.setData(pos=pos, adj=adj, pen=lines, size=1, symbol=symbols, pxMode=False, text=texts)
+        # self.vLine = pg.InfiniteLine(angle=90, movable=False)
+        # self.hLine = pg.InfiniteLine(angle=0, movable=False)
+        # self.v.addItem(self.vLine, ignoreBounds=True)
+        # self.v.addItem(self.hLine, ignoreBounds=True)
+        # self.linepen = pg.mkPen({'color': "111"})
+        # for i in range(1,192):
+        #     vlines = pg.InfiniteLine(angle=90, movable=False,pen=self.linepen)
+        #     self.v.addItem(vlines)
+        #     vlines.setPos([i,0])
+
+        self.vb = CustomViewBox()
+        self.vb.setAspectLocked(False)
+        self.pw = pg.PlotWidget(viewBox=self.vb, enableMenu=True,
+                           title="Plot 2dd L2G Curve<br>left-drag to zoom, right-click to reset zoom")
+        self.pw.showGrid(True,True,0.5)
+        self.ui.gridLayout_2.addWidget(self.pw, 0, 0)
+        # self.r = pg.CustomPolyLineROI([(0, 1),  (10,1), (20,1), (30, 1),(2570,0x800)])
+        self.generateLGdata()
+        self.r = CustomPolyLineROI2(self.lgdata)
+        self.vb.addItem(self.r)
+        # self.vLine = pg.InfiniteLine(angle=90, movable=False)
+        # self.hLine = pg.InfiniteLine(angle=0, movable=False)
+        # self.vb.addItem(self.vLine, ignoreBounds=True)
+        # self.vb.addItem(self.hLine, ignoreBounds=True)
+
+        # proxy = pg.SignalProxy(self.vb.scene().sigMouseMoved, rateLimit=60, slot=self.mouseMoved)
+
+        self.timer = pg.QtCore.QTimer()
+        self.timer.timeout.connect(self.update)
+        # self.timer.start(2000)
+
+    def setConnectFlag(self, flag):
+        self.connectFlag = flag
+
+    def update(self):
+        print self.r.getState()
+        # self.vLine.setPos(self.r.getState()["points"][0])
+        # self.hLine.setPos(self.r.getState()["points"][0])
+        pass
+
+    def generateLGdata(self):
+        for i in range(11,-1,-1):
+            j = (i*20, 0x400+(0xC00-0x400)*(11-i)/11)
+            self.lgdata.append(j)
+
+        print self.lgdata
+        pass
+
+
+
+if __name__ == "__main__":
+    app = QtGui.QApplication(sys.argv)
+    mywindow = DrawL2GWindow()
+    mywindow.show()
+    sys.exit(app.exec_())
---
+++ twoFirst01/DimmingDebugger_Readback_SX7/DrawMap_res.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'DrawMap_res.ui'
+#
+# Created: Wed Jul 12 14:08:08 2017
+#      by: PyQt4 UI code generator 4.9.6
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt4 import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_Form_Hist(object):
+    def setupUi(self, Form_Hist):
+        Form_Hist.setObjectName(_fromUtf8("Form_Hist"))
+        Form_Hist.resize(1187, 708)
+        self.tableWidget_lg = QtGui.QTableWidget(Form_Hist)
+        self.tableWidget_lg.setGeometry(QtCore.QRect(10, 10, 391, 261))
+        self.tableWidget_lg.setRowCount(8)
+        self.tableWidget_lg.setColumnCount(8)
+        self.tableWidget_lg.setObjectName(_fromUtf8("tableWidget_lg"))
+        self.tableWidget_lg.horizontalHeader().setVisible(False)
+        self.tableWidget_lg.horizontalHeader().setDefaultSectionSize(45)
+        self.tableWidget_lg.horizontalHeader().setHighlightSections(False)
+        self.tableWidget_lg.horizontalHeader().setMinimumSectionSize(21)
+        self.tableWidget_lg.verticalHeader().setVisible(False)
+        self.gridLayoutWidget = QtGui.QWidget(Form_Hist)
+        self.gridLayoutWidget.setGeometry(QtCore.QRect(30, 360, 160, 80))
+        self.gridLayoutWidget.setObjectName(_fromUtf8("gridLayoutWidget"))
+        self.gridLayout = QtGui.QGridLayout(self.gridLayoutWidget)
+        self.gridLayout.setMargin(0)
+        self.gridLayout.setObjectName(_fromUtf8("gridLayout"))
+        self.pushButton_refresh = QtGui.QPushButton(self.gridLayoutWidget)
+        self.pushButton_refresh.setObjectName(_fromUtf8("pushButton_refresh"))
+        self.gridLayout.addWidget(self.pushButton_refresh, 0, 0, 1, 1)
+        self.pushButton_1 = QtGui.QPushButton(self.gridLayoutWidget)
+        self.pushButton_1.setObjectName(_fromUtf8("pushButton_1"))
+        self.gridLayout.addWidget(self.pushButton_1, 1, 0, 1, 1)
+        self.pushButton_2 = QtGui.QPushButton(self.gridLayoutWidget)
+        self.pushButton_2.setObjectName(_fromUtf8("pushButton_2"))
+        self.gridLayout.addWidget(self.pushButton_2, 0, 1, 1, 1)
+        self.pushButton_3 = QtGui.QPushButton(self.gridLayoutWidget)
+        self.pushButton_3.setObjectName(_fromUtf8("pushButton_3"))
+        self.gridLayout.addWidget(self.pushButton_3, 1, 1, 1, 1)
+        self.label_1 = QtGui.QLabel(Form_Hist)
+        self.label_1.setGeometry(QtCore.QRect(40, 300, 54, 12))
+        self.label_1.setObjectName(_fromUtf8("label_1"))
+        self.label_2 = QtGui.QLabel(Form_Hist)
+        self.label_2.setGeometry(QtCore.QRect(160, 300, 54, 12))
+        self.label_2.setObjectName(_fromUtf8("label_2"))
+        self.gridLayoutWidget_2 = QtGui.QWidget(Form_Hist)
+        self.gridLayoutWidget_2.setGeometry(QtCore.QRect(430, 10, 741, 681))
+        self.gridLayoutWidget_2.setObjectName(_fromUtf8("gridLayoutWidget_2"))
+        self.gridLayout_2 = QtGui.QGridLayout(self.gridLayoutWidget_2)
+        self.gridLayout_2.setMargin(0)
+        self.gridLayout_2.setObjectName(_fromUtf8("gridLayout_2"))
+
+        self.retranslateUi(Form_Hist)
+        QtCore.QMetaObject.connectSlotsByName(Form_Hist)
+
+    def retranslateUi(self, Form_Hist):
+        Form_Hist.setWindowTitle(_translate("Form_Hist", "Form_Hist", None))
+        self.pushButton_refresh.setText(_translate("Form_Hist", "Refresh", None))
+        self.pushButton_1.setText(_translate("Form_Hist", "PushButton", None))
+        self.pushButton_2.setText(_translate("Form_Hist", "PushButton", None))
+        self.pushButton_3.setText(_translate("Form_Hist", "PushButton", None))
+        self.label_1.setText(_translate("Form_Hist", "TextLabel", None))
+        self.label_2.setText(_translate("Form_Hist", "TextLabel", None))
+
