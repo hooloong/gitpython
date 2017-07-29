76798872fc91239e90ef31f5cf63f28f9e9f7cc9
hooloong <hooloong@vip.qq.com>
2017-07-29 12:20:38 +0800

    111111

---
+++ twoFirst01/opencv/cv001.py
@@ -0,0 +1,14 @@
+__author__ = 'hooloongge'
+# -*- coding: utf-8 -*-
+import sys, random,string,struct,json
+import numpy as np
+import csv
+from PyQt4 import QtCore, QtGui, uic
+import cv2
+
+
+img = cv2.imread('code.jpg')
+
+print img
+
+
