604d4dc38fdd6c091c51d417d1900b05d4ebe490
hooloong <hooloong@vip.qq.com>
2017-07-31 21:00:38 +0800

    111111

--- twoFirst01/opencv/cv001.py
+++ twoFirst01/opencv/cv001.py
@@ -7,8 +7,13 @@ from PyQt4 import QtCore, QtGui, uic
 import cv2
 
 
-img = cv2.imread('code.jpg')
+# img = cv2.imread('code.jpg')
+img = np.zeros((3,3),dtype=np.uint8)
+img = cv2.cvtColor(img,cv2.COLOR_GRAY2BGR)
 
+print img.shape
 print img
+
+
 
 
