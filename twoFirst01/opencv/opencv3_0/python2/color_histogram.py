c7b7d0ba49e430180f0eb06b25c136bddc88565c
hooloong <hooloongge@sigmadesigns.com>
2017-08-10 17:37:25 +0800

    30 examples

--- twoFirst01/opencv/opencv3_0/python2/color_histogram.py
+++ twoFirst01/opencv/opencv3_0/python2/color_histogram.py
@@ -13,7 +13,9 @@ import video
 if __name__ == '__main__':
 
     hsv_map = np.zeros((180, 256, 3), np.uint8)
+
     h, s = np.indices(hsv_map.shape[:2])
+
     hsv_map[:,:,0] = h
     hsv_map[:,:,1] = s
     hsv_map[:,:,2] = 255
